eb2eadf249d37d1135f4ba3508dd3cf579cab0ac
Max Korotieiev <maxkorotieiev@gmail.com>
2018-05-12 13:43:02 +0300

    Initial commit

---
+++ src/com/company/Girl.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Girl {
+    private String name;
+    private int age;
+
+    public Girl(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Girl() {
+        this("Ket", 33);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Go to Mac");
+    }
+}
